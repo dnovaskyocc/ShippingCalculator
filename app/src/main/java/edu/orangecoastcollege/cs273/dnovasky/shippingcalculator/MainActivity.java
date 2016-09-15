e218bc9b4e95e8b3b5ad06fa62326fcc000a70f4
dnovaskyocc <donnov48@gmail.com>
2016-09-15 12:13:58 -0700

    Finished Project #1

--- app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/shippingcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/shippingcalculator/MainActivity.java
@@ -22,28 +22,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize our member variables
         mBaseCost = (TextView) findViewById(R.id.baseCurrencyTextView);
         mAddedCost = (TextView) findViewById(R.id.addedCurrencyTextView);
         mTotalCost = (TextView) findViewById(R.id.totalCurrencyTextView);
 
+        // Initialize our item to be sipped
         mItem = new ShipItem();
+
+        // Create our TextWatcher to listen for changes in the package weight
         TextWatcher textWatcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                // Do nothing
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                // Create a NumberFormat object to set the currency to the current locale
                 NumberFormat format = NumberFormat.getCurrencyInstance();
+
+                // Check if the mWeightTeField is empty.
                 if(isEmpty(mWeightField))
                 {
+                    // It's empty so we display $0.00 for all cost
                     mBaseCost.setText(R.string.zero_dollars);
                     mAddedCost.setText(R.string.zero_dollars);
                     mTotalCost.setText(R.string.zero_dollars);
                 }
                 else
                 {
+                    // It's not empty so we update display accordingly
                     mItem.setWeight(Integer.parseInt(charSequence.toString()));
                     mBaseCost.setText(format.format(mItem.getBaseCost()));
                     mAddedCost.setText(format.format(mItem.getAddedCost()));
@@ -53,15 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                // Do nothing
             }
         };
 
+        // Connect our edit text member variable to our edit text in the UI
         mWeightField = (EditText) findViewById(R.id.packageWeightEditText);
 
+        // Assert that our EditText field has been initialized
         assert mWeightField != null;
+        // Add our text watcher to listen for changes
         mWeightField.addTextChangedListener(textWatcher);
     }
+
+    /**
+     * Check to see if our EditText field is empty
+     * @param etText the Edit Text field we ant to check
+     * @return true if its empty false otherwise
+     */
     private boolean isEmpty(EditText etText) {
         return etText.getText().toString().trim().length() == 0;
     }
--- app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/shippingcalculator/ShipItem.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/shippingcalculator/ShipItem.java
@@ -13,32 +13,54 @@ public class ShipItem {
     private double mAddedCost;
     private double mTotalCost;
 
+    /**
+     * Creates a package to be shipped and sets the base cost to $3.00
+     * all other fields are defaulted to 0.
+     */
     public ShipItem()
     {
         mBaseCost = 3.0;
+        mWeight = 0;
+        mAddedCost = 0;
+        mTotalCost = 0;
     }
 
+    /**
+     * Gets the base cost of the package
+     * @return mBaseCost this returns the base cost of the package
+     */
     public double getBaseCost() {
         return mBaseCost;
     }
 
+    /**
+     * Gets the added cost of the package
+     * @return mAddedCost this returns the cost of the package
+     */
     public double getAddedCost() {
         return mAddedCost;
     }
 
+    /**
+     * Gets the total cost of the package
+     * @return mTotalCost this return the total cost of the packge
+     */
     public double getTotalCost() {
         return mTotalCost;
     }
 
+    /**
+     * Changes the weight of the package. And adjust the base and
+     * cost accordingly.
+     * @param weight the packages new weight
+     */
     public void setWeight(int weight) {
 
         mWeight = weight;
         mAddedCost = 0;
 
         if(mWeight == 0)
-        {
             mBaseCost = 0;
-        }
         else
         {
             mBaseCost = 3.0;
